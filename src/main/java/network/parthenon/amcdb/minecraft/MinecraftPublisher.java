dc63aeac75da7ee6f9b5b5777de363ae875003ff
0x4e49434f4c45 <>
2022-12-29 14:14:18 -0800

    Implement command execution via console channel

--- src/main/java/network/parthenon/amcdb/minecraft/MinecraftPublisher.java
+++ src/main/java/network/parthenon/amcdb/minecraft/MinecraftPublisher.java
@@ -1,6 +1,7 @@
 package network.parthenon.amcdb.minecraft;
 
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
 import network.parthenon.amcdb.AMCDB;
 import network.parthenon.amcdb.messaging.InternalMessage;
@@ -19,8 +20,10 @@ public class MinecraftPublisher implements MessageHandler {
         if(message.getType() == InternalMessage.MessageType.CHAT) {
             Text minecraftText = MinecraftFormatter.toMinecraftText(message);
             MinecraftService.getInstance().addRecentlyPublished(minecraftText.getString());
-            server.getPlayerManager()
-                    .broadcast(minecraftText, false);
+            server.getPlayerManager().broadcast(minecraftText, false);
+        }
+        else if(message.getType() == InternalMessage.MessageType.CONSOLE) {
+            server.getCommandManager().executeWithPrefix(server.getCommandSource(), message.toString());
         }
     }
 
